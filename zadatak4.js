06f3c0875fe04d7893334d9133c9e8d8ebb26e91
Milan Jacimovic <jacimovicmilanfon@gmail.com>
2020-02-04 13:29:04 +0100

    dodati nacini

--- zadatak4.js
+++ zadatak4.js
@@ -1,27 +1,31 @@
-// `let x= 5;
+// Prvi nacin
+//  `let x= 5;
 
 
-// for (let i=5; i<1000*x; i++) { 
+// Drugi nacin
+//for (let i=5; i<1000*x; i++) { 
 //     if (i%x==0){
 //     console.log(i);
    
 // }
 // }`
 
-
+// Drugi nacin
 // for (let m=5; m<5000; m++) { 
-//     if (m%x==0){
+//     if (m%5==0){
 //     console.log(m);
    
 // }
 // }
 
 
-// let brojacdo1000 = 0
-// for (let brojkojisedeli=5; brojacdo1000<1000; brojkojisedeli++) { 
-//     if (brojkojisedeli%5==0){
-//     console.log(brojkojisedeli);
-//     brojacdo1000++;
-// }
-// }
+// Treci nacin
+
+let brojacdo1000 = 0
+for (let brojkojisedeli=5; brojacdo1000<1000; brojkojisedeli++) { 
+    if (brojkojisedeli%5==0){
+    console.log(brojkojisedeli);
+    brojacdo1000++;
+}
+}
 
